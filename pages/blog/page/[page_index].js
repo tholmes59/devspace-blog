7a9fdc8c91b88a296c36b623f2abdee7a21a8c68
Thomas <tholmes59@gmail.com>
2022-10-18 10:43:43 -0700

    create props for number of pages and current page in order to implement pagination

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -7,7 +7,7 @@ import Post from "../../../components/Post";
 import { sortByDate } from "../../../utils";
 import { POSTS_PER_PAGE } from "../../../config";
 
-export default function BlogPage({ posts }) {
+export default function BlogPage({ posts, numPages, currentPage }) {
   return (
     <Layout>
       <h1 className="text-5xl border-b-4 p-5">Blog</h1>
@@ -39,7 +39,9 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ params }) {
+  const page = parseInt((params && params.page_index) || 1);
+
   const files = fs.readdirSync(path.join("posts"));
 
   const posts = files.map((filename) => {
@@ -58,9 +60,17 @@ export async function getStaticProps() {
     };
   });
 
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
+  const pageIndex = page - 1;
+  const orderedPosts = posts
+    .sort(sortByDate)
+    .slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE);
+
   return {
     props: {
-      posts: posts.sort(sortByDate),
+      posts: orderedPosts,
+      numPages,
+      currentPage: page,
     },
   };
 }
