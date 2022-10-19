aa6edd260fe97870f2e7710c0bcd1596143348cc
Thomas <tholmes59@gmail.com>
2022-10-19 10:04:30 -0700

    update import paths with new js config set up

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -3,10 +3,10 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import Link from "next/link";
-import Post from "../../../components/Post";
-import { sortByDate } from "../../../utils";
-import { POSTS_PER_PAGE } from "../../../config";
-import Pagination from "../../../components/Pagination";
+import Post from "@/components/Post";
+import { sortByDate } from "@/utils/index";
+import { POSTS_PER_PAGE } from "@/config/index";
+import Pagination from "@/components/Pagination";
 
 export default function BlogPage({ posts, numPages, currentPage }) {
   return (
