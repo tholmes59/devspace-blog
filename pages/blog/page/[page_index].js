19c7e80a0769c2d3cac7b069660bc5ed14b75229
Thomas <tholmes59@gmail.com>
2022-10-19 09:41:50 -0700

    import pagination component and pass props for page number and number of pages

--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -6,6 +6,7 @@ import Link from "next/link";
 import Post from "../../../components/Post";
 import { sortByDate } from "../../../utils";
 import { POSTS_PER_PAGE } from "../../../config";
+import Pagination from "../../../components/Pagination";
 
 export default function BlogPage({ posts, numPages, currentPage }) {
   return (
@@ -17,6 +18,8 @@ export default function BlogPage({ posts, numPages, currentPage }) {
           <Post key={index} post={post} />
         ))}
       </div>
+
+      <Pagination currentPage={currentPage} numPages={numPages} />
     </Layout>
   );
 }
