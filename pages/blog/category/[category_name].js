f0bd183e127539fd8f66137e58b27e8290eedc98
Thomas <tholmes59@gmail.com>
2022-10-21 09:47:40 -0700

    clean up repeated code by rplacing parts of getStaticProps function with getPosts from lib file

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -2,9 +2,8 @@ import Layout from "@/components/Layout";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import Link from "next/link";
 import Post from "@/components/Post";
-import { sortByDate } from "@/utils/index";
+import { getPosts } from "@/lib/post";
 
 export default function CategoryBlogPage({ posts, categoryName }) {
   return (
@@ -47,21 +46,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { category_name } }) {
   const files = fs.readdirSync(path.join("posts"));
 
-  const posts = files.map((filename) => {
-    const slug = filename.replace(".md", "");
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join("posts", filename),
-      "utf-8"
-    );
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug,
-      frontmatter,
-    };
-  });
+  const posts = getPosts();
 
   //Filter posts by category
   const categoryPosts = posts.filter(
@@ -70,7 +55,7 @@ export async function getStaticProps({ params: { category_name } }) {
 
   return {
     props: {
-      posts: categoryPosts.sort(sortByDate),
+      posts: categoryPosts,
       categoryName: category_name,
     },
   };
