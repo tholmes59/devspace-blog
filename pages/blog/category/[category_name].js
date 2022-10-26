ba32bd355f00eec6d76846eaa9bdb34ba7517356
Thomas <tholmes59@gmail.com>
2022-10-26 10:28:22 -0700

    create props for category names and import category list to display clickable sidebar of each blog post categoreies

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -1,19 +1,27 @@
 import Layout from "@/components/Layout";
+import CategoryList from "@/components/CategoryList";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import Post from "@/components/Post";
 import { getPosts } from "@/lib/post";
 
-export default function CategoryBlogPage({ posts, categoryName }) {
+export default function CategoryBlogPage({ posts, categoryName, categories }) {
   return (
     <Layout>
-      <h1 className="text-5xl border-b-4 p-5">Posts in {categoryName}</h1>
+      <div className="flex justify-between">
+        <div className="w-3/4 mr-10">
+          <h1 className="text-5xl border-b-4 p-5">Posts in {categoryName}</h1>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+        </div>
+        <div className="w-1/4">
+          <CategoryList categories={categories} />
+        </div>
       </div>
     </Layout>
   );
@@ -48,6 +56,11 @@ export async function getStaticProps({ params: { category_name } }) {
 
   const posts = getPosts();
 
+  //Get categories for sidebar
+  const categories = posts.map((post) => post.frontmatter.category);
+
+  const uniqueCategories = [...new Set(categories)];
+
   //Filter posts by category
   const categoryPosts = posts.filter(
     (post) => post.frontmatter.category.toLowerCase() === category_name
@@ -57,6 +70,7 @@ export async function getStaticProps({ params: { category_name } }) {
     props: {
       posts: categoryPosts,
       categoryName: category_name,
+      categories: uniqueCategories,
     },
   };
 }
