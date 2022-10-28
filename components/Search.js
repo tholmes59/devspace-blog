d4edbd7b23e88195a8c5e32fdce4224c3fd5ae84
Thomas <tholmes59@gmail.com>
2022-10-28 15:54:07 -0700

    create search component

---
+++ components/Search.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { faSearch } from "react-icons";
+
+export default function Search() {
+  return <div>Search</div>;
+}
