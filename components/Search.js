f0fccba1eb499e60e5d35179b2e8c8e74d5270e8
Thomas <tholmes59@gmail.com>
2022-11-02 08:44:10 -0700

    create useEffect hook to pull posts based on search term inputs

--- components/Search.js
+++ components/Search.js
@@ -1,10 +1,24 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
+import SearchResults from "./SearchResults";
 
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    const getResults = async () => {
+      if (searchTerm === "") {
+        setSearchResults([]);
+      } else {
+        const res = await fetch(`/api/search?q=${searchTerm}`);
+        const { results } = await res.json();
+        setSearchResults(results);
+      }
+    };
+
+    getResults();
+  }, [searchTerm]);
 
   return (
     <div className="relative bg-gray-600 p-4">
@@ -18,12 +32,15 @@ export default function Search() {
               className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none w-72"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              placeholder="Search Posts"
+              placeholder="Search Posts..."
             />
+
             <FaSearch className="absolute top-0 right-0 text-black mt-3 mr-4" />
           </form>
         </div>
       </div>
+
+      <SearchResults results={searchResults} />
     </div>
   );
 }
